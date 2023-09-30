378cdbd8d12030c11efa5f26495c8a9d8305c6fc
Ewoma Ozore <ewomaozore@gmail.com>
2023-09-30 20:19:05 +0100

    initial commit

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Layout, theme } from 'antd';
-import { FileTextOutlined, SearchOutlined, DownOutlined } from '@ant-design/icons';
+import { DownOutlined, FileTextOutlined, SearchOutlined } from '@ant-design/icons';
 import { Input } from 'antd';
-import type { MenuProps } from 'antd';
+import { MenuProps } from 'antd';
 import { Dropdown, Space, Typography } from 'antd';
 import RightContent from './RightContent';
 
@@ -15,19 +15,122 @@ const items: MenuProps['items'] = [
     },
 ];
 
+type Candidate = {
+    name: string;
+    state: string;
+    country: string;
+    education: string;
+    hashtag: string;
+};
+
+const candidates: Candidate[] = [
+    {
+        name: 'Aaliyah Sanderson',
+        state: 'Lagos',
+        country: 'Nigeria',
+        education: 'Bachelor - Cambridge University (2023 - 2023)',
+        hashtag: 'top_candidate',
+    },
+    {
+        name: 'John Doe',
+        state: 'New York',
+        country: 'USA',
+        education: 'Master - Harvard University (2022 - 2024)',
+        hashtag: 'high_achiever',
+    },
+    {
+        name: 'Jane Smith',
+        state: 'London',
+        country: 'UK',
+        education: 'PhD - Oxford University (2021 - 2025)',
+        hashtag: 'scholar',
+    },
+    {
+        name: 'Michael Johnson',
+        state: 'Los Angeles',
+        country: 'USA',
+        education: 'Bachelor - Stanford University (2023 - 2023)',
+        hashtag: 'top_performer',
+    },
+    {
+        name: 'Sara Brown',
+        state: 'Sydney',
+        country: 'Australia',
+        education: 'Master - University of Melbourne (2022 - 2024)',
+        hashtag: 'achiever',
+    },
+    {
+        name: 'David Wilson',
+        state: 'Toronto',
+        country: 'Canada',
+        education: 'Bachelor - University of Toronto (2023 - 2023)',
+        hashtag: 'talented',
+    },
+    {
+        name: 'Emma Johnson',
+        state: 'Chicago',
+        country: 'USA',
+        education: 'Master - Northwestern University (2022 - 2024)',
+        hashtag: 'exceptional',
+    },
+    {
+        name: 'William Davis',
+        state: 'Berlin',
+        country: 'Germany',
+        education: 'Bachelor - Humboldt University (2023 - 2023)',
+        hashtag: 'outstanding',
+    },
+    {
+        name: 'Olivia Martin',
+        state: 'Paris',
+        country: 'France',
+        education: 'PhD - Sorbonne University (2021 - 2025)',
+        hashtag: 'stellar',
+    },
+    {
+        name: 'Ethan Clark',
+        state: 'Tokyo',
+        country: 'Japan',
+        education: 'Bachelor - University of Tokyo (2023 - 2023)',
+        hashtag: 'promising',
+    },
+    // Add more candidates here
+];
+
 const Body = () => {
     const {
         token: { colorBgContainer },
     } = theme.useToken();
+
+    const [searchQuery, setSearchQuery] = useState<string>('');
+    const [filteredData, setFilteredData] = useState<Candidate[]>(candidates);
+
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const query = e.target.value;
+        setSearchQuery(query);
+
+        const filteredCandidates = candidates.filter((candidate) =>
+            candidate.name.toLowerCase().includes(query.toLowerCase())
+        );
+
+        setFilteredData(filteredCandidates);
+    };
+
     return (
-        <div className='flex'>
+        <div className="flex">
             <Content style={{ margin: '0px 16px 0', overflow: 'initial' }}>
-                <Input size="large" placeholder="Search by name, edu, exp or #tag" className='mb-5' prefix={<SearchOutlined />} />
-                <div className='flex justify-between bg-white px-5 py-5 border border-b-gray-300'>
-                    <p className='font-semibold text-xl text-black'>Filters</p>
+                <Input
+                    size="large"
+                    placeholder="Search by name, edu, exp, or #tag"
+                    className="mb-5"
+                    prefix={<SearchOutlined />}
+                    onChange={handleSearch}
+                />
+                <div className="flex justify-between bg-white px-5 py-5 border border-b-gray-300">
+                    <p className="font-semibold text-xl text-black">Filters</p>
                     <p>0 Selected</p>
                 </div>
-                <div className='rounded-lg' style={{ textAlign: 'left', background: colorBgContainer }}>
+                <div className="rounded-lg" style={{ textAlign: 'left', background: colorBgContainer }}>
                     <div style={{ flex: '40%' }}>
                         <Dropdown
                             menu={{
@@ -117,10 +220,10 @@ const Body = () => {
                 </div>
             </Content>
             <div style={{ flex: '60%', marginRight: '20px' }}>
-                <RightContent />
+                <RightContent candidates={searchQuery ? filteredData : candidates} />
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Body;
